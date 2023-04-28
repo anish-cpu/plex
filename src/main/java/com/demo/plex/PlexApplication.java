e9dc6361ba26d752c7a95ca34bf2f112b4b53277
anish.chowdhury <anish.chowdhury@payu.in>
2023-04-28 21:10:53 +0530

    basic dependencies

--- src/main/java/com/demo/plex/PlexApplication.java
+++ src/main/java/com/demo/plex/PlexApplication.java
@@ -1,9 +1,18 @@
 package com.demo.plex;
 
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.data.envers.repository.support.EnversRevisionRepositoryFactoryBean;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 @SpringBootApplication
+@ComponentScan({"com.demo.plex"})
+@EnableJpaRepositories(value = {"com.demo.plex" }, repositoryFactoryBeanClass = EnversRevisionRepositoryFactoryBean.class)
+@EntityScan({"com.demo.plex" })
+@EnableAutoConfiguration
 public class PlexApplication {
 
 	public static void main(String[] args) {
